f2ff013255369a305e239d66bca39c523b7f88ba
Imtiazkun <alshahriar.me@gmail.com>
2025-01-24 18:47:22 +0600

    Updated landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 interface ApiResponse {
   data: Array<{
     id: number;
@@ -35,7 +37,60 @@ export default async function Home() {
   const segments: ApiResponse = await data.json();
 
   return (
-    <div className="h-screen w-full">
+    <div className="min-h-screen w-full">
+      <div
+        style={{
+          backgroundImage: "url(/noise.png)",
+          backgroundSize: "cover",
+          backgroundPosition: "top center",
+        }}
+        className="w-full h-screen relative"
+      >
+        <div className="flex items-center justify-center gap-10 container mx-auto px-5 lg:px-20 ">
+          <div className="flex-1 py-10">
+            <Image
+              width={800}
+              height={600}
+              src="/banner.png"
+              alt=""
+              className="w-full"
+            />
+          </div>
+          <div className="flex-1 text-white">
+            <p className="code text-6xl font-bold text-right">00: 23 : 51</p>
+            <p className="text-right text-2xl">
+              <small className="font-bold code">Registration ends in</small>
+            </p>
+            <p className="text-justify text-xl">
+              Rose from the pits of hell, rise shall we again. With the
+              forthcoming advent of Traction অভ্যুদয়, we, the Robotics Club of
+              BRAC University, celebrate the revolution with yet another
+              platform for you to channel the revolutionary in you.
+            </p>
+            <div>
+              <a className="bg-white w-full font-black text-black mt-2 py-4 rounded-lg flex items-center justify-center text-xl gap-2 tracking-widest hover:bg-black transition-colors cursor-pointer hover:text-white">
+                Register{" "}
+                <Image
+                  width={50}
+                  height={50}
+                  src="redirect.svg"
+                  className="w-7"
+                  alt=""
+                />
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="flex-1 flex items-center justify-center relative select-none">
+          <Image
+            width={1472}
+            height={832}
+            src="/astronaut.png"
+            alt=""
+            className="object-contain absolute -top-14 -z-0 up levitate"
+          />
+        </div>
+      </div>
       <div className="min-h-screen container mx-auto px-5 xl:px-20 py-10">
         <h2>Segments</h2>
         <div>
