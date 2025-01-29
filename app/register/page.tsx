c543724e511647d5da0f09d23613e41e6d423438
Imtiazkun <alshahriar.me@gmail.com>
2025-01-29 08:31:55 +0600

    Added Payment info

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -94,6 +94,11 @@ const segments = [
       "Think of FIFA WORLD CUP, but more futuristic! Robots on the field with strategies sharper than Messi's free kick. The flawless blend of engineering and competitive minds. Two Bots, one ball, unlimited action, powered by the skilled remote handlers or maybe even autonomously. This segment will bring robotic godzillas that will clash on the field fighting to score the perfect goal and bring the trophy with honor. Will your Bot be the next 'Messi'? Join this tournament to find the potential of you and your Bot-footballer.",
     fee: "1200 BDT for 1-3 Members, 400 BDT for additional member (Max 5 Members)",
     rulebook_link: "https://tinyurl.com/muetffnj",
+    payment: {
+      rep_name: "Zabid Rahman | Senior Executive, Finance & Marketing",
+      rep_fb: "https://www.facebook.com/zrasif.zrasif?mibextid=ZbWKwL",
+      bKash: "01537027689",
+    },
   },
   {
     id: 2,
@@ -103,6 +108,11 @@ const segments = [
       "Ever dreamed of being an F1 driver, as well as own a car like one? Buckle up racers, it's time to put your mind on building up the engines and wheels for your Bot, which can accelerate faster on tracks. The path for achieving the award is set and the time is running out. All you have to do is to create a bot which can follow the line, navigating the turns with sharp precision with furious speed. Is your Bot going to be the next F1 driver's car?",
     fee: "1200 BDT for 1-3 Members, 400 BDT for additional member (Max 5 Members)",
     rulebook_link: "https://tinyurl.com/mrnppcm4",
+    payment: {
+      rep_name: "Zabid Rahman | Senior Executive, Finance & Marketing",
+      rep_fb: "https://www.facebook.com/zrasif.zrasif?mibextid=ZbWKwL",
+      bKash: "01537027689",
+    },
   },
   {
     id: 3,
@@ -112,6 +122,12 @@ const segments = [
       "This isn't just a competition about following a path, here a bot has to precisely navigate a complex line-marked path while proving its algorithms can be the boss in motion. This segment is the prime example where programming meets precision parking. In a country where taking the right path isn’t just a robotics challenge but a life lesson, Pathfinder is the perfect segment of the event to challenge your bots problem solving skills of precision on which path to take and ultimately finishing the quest. Will your LFR hold the title of master of the course, or will it get stuck in a never ending loop?",
     fee: "1200 BDT for 1-3 Members, 400 BDT for additional member (Max 5 Members)",
     rulebook_link: "https://tinyurl.com/24y2tata",
+    payment: {
+      rep_name:
+        "Tasnia Mahjabin Maliha | Senior Executive, Finance & Marketing",
+      rep_fb: "https://www.facebook.com/share/1Er5wzvvjx/?mibextid=LQQJ4d",
+      bKash: "01310770109",
+    },
   },
   {
     id: 4,
@@ -121,6 +137,12 @@ const segments = [
       "Dive into the ultimate aquatic showdown! Picture your bot cutting through waves like a rogue aquatic machine outmaneuvering every challenge. Aqua Wars is where the challenges of robotics and maneuvering water bodies join together. This segment challenges your engineering capacity to design a bot that can not only race but also navigate unpredictable aquatic terrains. Competitors will design water surface robots that has good speed, maneuvering capacity and stability to stand against high velocity and turbulence of water. Will your robot dominate the waves and claim the Aqua Wars championship? Get ready to ride the tides!",
     fee: "1200 BDT for 1-3 Members, 400 BDT for additional member (Max 5 Members)",
     rulebook_link: "https://tinyurl.com/2sy9dhcr",
+    payment: {
+      rep_name:
+        "Tasnia Mahjabin Maliha | Senior Executive, Finance & Marketing",
+      rep_fb: "https://www.facebook.com/share/1Er5wzvvjx/?mibextid=LQQJ4d",
+      bKash: "01310770109",
+    },
   },
   {
     id: 5,
@@ -130,6 +152,11 @@ const segments = [
       "Step into the realm of boundless creativity powered by AI! This segment puts your skills to test as you design functional, creative websites by crafting prompts that explores full potential of ChatGPT. Use GPT to build frameworks, enhance designs or solve challenges in real time. From sleek interfaces to innovative features, from responsive layouts to seamless user experiences let your creativity and AI's power merge to redefine website building. The rebellion begins with your code - are you ready?",
     fee: "500 BDT",
     rulebook_link: "https://tinyurl.com/3j87syb6",
+    payment: {
+      rep_name: "Shayonto Rayhan | Senior Executive, Finance & Marketing",
+      rep_fb: "https://www.facebook.com/share/19fLtXZmht/?mibextid=wwXIfr",
+      bKash: "01309029431",
+    },
   },
   {
     id: 6,
@@ -139,6 +166,11 @@ const segments = [
       "Bring your vision to life and share it with the world. The Innovators Exhibit aims to give you the stage to show your projects that redefine technology. Whether it’s a game-changing prototype, solution to a real-world problem, or a glimpse into the future - this is your moment. Let your ideas take the spotlight and show the world what you envisioned, designed and built. This is your chance to lead the charge into a new era of technology. Could your idea become the next big thing? Join us to discover your full potential!",
     fee: "1200 BDT for 1-3 Members, 400 BDT for 2 additional member each (Max 5 Members)",
     rulebook_link: "https://tinyurl.com/3t7cy2k2",
+    payment: {
+      rep_name: "Shayonto Rayhan | Senior Executive, Finance & Marketing",
+      rep_fb: "https://www.facebook.com/share/19fLtXZmht/?mibextid=wwXIfr",
+      bKash: "01309029431",
+    },
   },
 ];
 
@@ -296,13 +328,6 @@ const TeamForm = () => {
           <div className="flex items-center flex-col lg:flex-row justify-center gap-10 container mx-auto px-5 lg:px-20 ">
             <div className="flex-1 md:py-10">
               <Image
-                width={800}
-                height={600}
-                src="/banner.png"
-                alt=""
-                className="w-full"
-              />
-              <Image
                 width={370}
                 height={104}
                 src="/sponsor.png"
@@ -312,7 +337,14 @@ const TeamForm = () => {
                   background: "rgba(255, 255, 255, 0.20)",
                   backdropFilter: "blur(7.349999904632568px)",
                 }}
-                className="levitate select-none -z-10 bg-white"
+                className="select-none -z-10 bg-white"
+              />
+              <Image
+                width={800}
+                height={600}
+                src="/banner.png"
+                alt=""
+                className="w-full"
               />
             </div>
             <div className="flex-1 text-white">
@@ -1419,6 +1451,48 @@ const TeamForm = () => {
             <>
               <Separator className="my-10" />
               <p>Payment</p>
+
+              <Card className="mt-5">
+                <CardHeader>
+                  <CardTitle>Payment</CardTitle>
+                  <CardDescription>
+                    {
+                      segments.filter(
+                        (item) => item.name == form.watch("segment")
+                      )[0].fee
+                    }
+                  </CardDescription>
+                </CardHeader>
+
+                <CardContent>
+                  <Link
+                    className="underline"
+                    target="_blank"
+                    href={
+                      segments.filter(
+                        (item) => item.name == form.watch("segment")
+                      )[0].payment?.rep_fb as string
+                    }
+                  >
+                    {
+                      segments.filter(
+                        (item) => item.name == form.watch("segment")
+                      )[0].payment?.rep_name
+                    }
+                  </Link>
+
+                  <p className="mt-2">
+                    <code className="px-2 py-1 mt-2 bg-gray-800 text-2xl rounded-sm">
+                      {
+                        segments.filter(
+                          (item) => item.name == form.watch("segment")
+                        )[0].payment?.bKash
+                      }
+                    </code>
+                  </p>
+                </CardContent>
+              </Card>
+
               <div className="flex w-full gap-5 items-start mt-5 flex-col lg:flex-row">
                 <FormField
                   control={form.control}
